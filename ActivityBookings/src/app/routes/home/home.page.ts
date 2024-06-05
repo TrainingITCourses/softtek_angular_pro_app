520df1202e3d9c7a19ddddd77bfa6d06b1a72857
Alberto Basalo <albertobasalo@hotmail.com>
2024-06-05 14:00:52 +0200

    feat: remove defer from server side

--- ActivityBookings/src/app/routes/home/home.page.ts
+++ ActivityBookings/src/app/routes/home/home.page.ts
@@ -1,6 +1,5 @@
-import { ChangeDetectionStrategy, Component, Signal, computed, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Signal, inject } from '@angular/core';
 import { Activity } from '@domain/activity.type';
-import { PlatformService } from '@services/platform.service';
 import { ActivityListComponent } from './activity-list.component';
 import { HomeService } from './home.service';
 import { SearchBarComponent } from './search-bar.component';
@@ -18,26 +17,18 @@ import { SearchBarComponent } from './search-bar.component';
   template: `
     <h1>Activities</h1>
     <lab-search-bar [placeholder]="'Search activities...'" (search)="onSearch($event)" />
-    @defer (when mustLoad()) {
-      <lab-activity-list [activities]="activities()" />
-    } @placeholder {
-      <cite> ⌛ Waiting for activities data</cite>
-    } @loading (minimum 1s) {
-      <cite> ⌛ Waiting for component instructions</cite>
-    }
+    <lab-activity-list [activities]="activities()" />
   `,
 })
 export default class HomePage {
   // * Injected services division
 
   #service: HomeService = inject(HomeService);
-  #platformService: PlatformService = inject(PlatformService);
 
   // * Public signal division
 
   /** List of activities */
   activities: Signal<Activity[]> = this.#service.activities;
-  mustLoad: Signal<boolean> = computed(() => this.#platformService.isServer || this.activities().length >= 0);
 
   // * event handlers division
 
